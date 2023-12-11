73c44946428f7943839ce643603181b80c18d79f
Mathieu VALERY <66632538+mathieu-valery@users.noreply.github.com>
2023-12-12 00:19:05 +0100

    fix credentials bug + delete rack sessions fix

--- escapades_backend/config/application.rb
+++ escapades_backend/config/application.rb
@@ -45,7 +45,5 @@ module EscapadesBackend
     config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
-
-    config.navigational_formats = []
   end
 end
