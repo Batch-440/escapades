bd54b188c17846f7db61cd02719244c26663612b
Mathieu VALERY <66632538+mathieu-valery@users.noreply.github.com>
2023-12-11 15:49:40 +0100

    burger menu done

--- escapades_frontend/src/components/Navbar/Navbar.tsx
+++ escapades_frontend/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,8 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import classes from "./Navbar.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 type NavBarLinkProps = {
   to: string;
@@ -12,22 +14,36 @@ const NavbarLink: FC<NavBarLinkProps> = ({ to, children }) => {
   const isActive = useMatch({ path: resolvedPath.pathname, end: true });
 
   return (
-    <li className={isActive ? classes.active : ""}>
+    <li className={isActive ? classes.Navbar__linkActive : ""}>
       <Link to={to}>{children}</Link>
     </li>
   );
 };
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const icon = isMenuOpen ? faXmark : faBars;
+  const toggleIsMenuOpen = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className={classes.Navbar}>
+    <nav className={`${classes.Navbar}`}>
       <Link to="/" className={classes.Navbar__title}>
         Escapades
       </Link>
-      <ul className={classes.Navbar__link}>
+      <ul
+        onClick={toggleIsMenuOpen}
+        className={`${classes.Navbar__link} ${
+          isMenuOpen ? classes.Navbar__open : ""
+        }`}
+      >
         <NavbarLink to="/register">register</NavbarLink>
         <NavbarLink to="/trip">organize a trip</NavbarLink>
       </ul>
+      <div className={classes.Navbar__icons}>
+        <FontAwesomeIcon onClick={toggleIsMenuOpen} icon={icon} />
+      </div>
     </nav>
   );
 };
