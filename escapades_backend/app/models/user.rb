8429d1374a63658895489725f71536a292e2a61e
Mathieu VALERY <66632538+mathieu-valery@users.noreply.github.com>
2023-12-18 17:23:54 +0100

    add country code to users

--- escapades_backend/app/models/user.rb
+++ escapades_backend/app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
   validates :password_confirmation, presence: true, on: :create
+  validates :country_code, inclusion: { in: ISO3166::Country.all.map(&:alpha2) }, :allow_nil => true
 
 end
