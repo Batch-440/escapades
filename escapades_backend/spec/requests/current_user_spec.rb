91409ddce203c1030bcdae3cb4997232fbf46905
Mathieu VALERY <66632538+mathieu-valery@users.noreply.github.com>
2023-12-23 17:26:39 +0100

    correct tests

--- escapades_backend/spec/requests/current_user_spec.rb
+++ escapades_backend/spec/requests/current_user_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 require 'devise/jwt/test_helpers'
 
-RSpec.describe 'User Login', type: :request do
+RSpec.describe 'Current User', type: :request do
   let(:user) { create(:user) }
 
-  it 'logs in an existing user and returns a bearer token' do
+  it 'current_user returns correct user' do
     auth_token = Devise::JWT::TestHelpers.auth_headers({}, user)['Authorization']
     get "/current_user", headers: { 'Authorization' => auth_token }
     expect(user).to have_attributes(JSON.parse(response.body))
