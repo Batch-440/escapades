356aaab14c6d7d23014b15f403563fc076512881
Mathieu VALERY <66632538+mathieu-valery@users.noreply.github.com>
2024-01-06 10:22:54 +0100

    rename const email in factory

--- escapades_backend/spec/factories/users.rb
+++ escapades_backend/spec/factories/users.rb
@@ -2,14 +2,14 @@ require 'faker'
 
 FactoryBot.define do
   before(:create) do
-   email = Faker::Internet.password
+   password = Faker::Internet.password
   end
 
   factory :user do
     email { Faker::Internet.email }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    password { email }
-    password_confirmation { email }
+    password { password }
+    password_confirmation { password }
   end
 end
